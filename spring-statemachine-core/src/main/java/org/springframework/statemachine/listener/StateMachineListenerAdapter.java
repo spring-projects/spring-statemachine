073df4e438919225733f79dcb76b6de94ba3d9ee
Janne Valkealahti <janne.valkealahti@gmail.com>
2015-03-21 15:19:17 +0000

    Add adapter impl for StateMachineListener

---
+++ spring-statemachine-core/src/main/java/org/springframework/statemachine/listener/StateMachineListenerAdapter.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.statemachine.listener;
+
+import org.springframework.statemachine.state.State;
+import org.springframework.statemachine.transition.Transition;
+
+/**
+ * Adapter implementation of {@link StateMachineListener} implementing all
+ * methods which extended implementation can override.
+ *
+ * @author Janne Valkealahti
+ *
+ * @param <S> the type of state
+ * @param <E> the type of event
+ */
+public class StateMachineListenerAdapter<S, E> implements StateMachineListener<S, E> {
+
+	@Override
+	public void stateChanged(State<S, E> from, State<S, E> to) {
+	}
+
+	@Override
+	public void stateEntered(State<S, E> state) {
+	}
+
+	@Override
+	public void stateExited(State<S, E> state) {
+	}
+
+	@Override
+	public void transition(Transition<S, E> transition) {
+	}
+
+	@Override
+	public void transitionStarted(Transition<S, E> transition) {
+	}
+
+	@Override
+	public void transitionEnded(Transition<S, E> transition) {
+	}
+
+}
