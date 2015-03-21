15a88b902af07d1f3b18146386b7434abd835f5e
Janne Valkealahti <janne.valkealahti@gmail.com>
2015-03-21 15:23:22 +0000

    Revert "updates"
    This reverts commit 7f11fdc8f97a12668c48ef452326d9943e1d890b.

--- spring-statemachine-samples/showcase/src/test/java/demo/showcase/ShowcaseTests.java
+++ spring-statemachine-samples/showcase/src/test/java/demo/showcase/ShowcaseTests.java
@@ -1,23 +1,11 @@
 package demo.showcase;
 
-import static org.hamcrest.Matchers.contains;
-import static org.junit.Assert.assertThat;
-
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.statemachine.EnumStateMachine;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.StateMachineSystemConstants;
-import org.springframework.statemachine.listener.StateMachineListener;
-import org.springframework.statemachine.listener.StateMachineListenerAdapter;
-import org.springframework.statemachine.state.State;
 
 import demo.CommonConfiguration;
 import demo.showcase.Application.Events;
@@ -27,75 +15,27 @@ public class ShowcaseTests {
 
 	private AnnotationConfigApplicationContext context;
 
-	private StateMachine<States,Events> machine;
-
-	private TestListener listener;
-
+	@SuppressWarnings("resource")
 	@Test
-	public void testInitialState() throws Exception {
-		listener.latch.await(1, TimeUnit.SECONDS);
-		assertThat(machine.getState().getIds(), contains(States.S0, States.S1, States.S11));
-	}
-
-	@Test
-	public void testA() throws Exception {
-		listener.reset(1);
-		machine.sendEvent(Events.A);
-		listener.latch.await(1, TimeUnit.SECONDS);
-		assertThat(machine.getState().getIds(), contains(States.S0, States.S1, States.S11));
-	}
-
-	@Test
-	public void testC() throws Exception {
-		listener.reset(1);
-		machine.sendEvent(Events.C);
-		listener.latch.await(1, TimeUnit.SECONDS);
-		assertThat(machine.getState().getIds(), contains(States.S0, States.S2, States.S21, States.S211));
-	}
-
-	static class Config {
-
-		@Autowired
-		private StateMachine<States,Events> machine;
-
-		@Bean
-		public StateMachineListener<States, Events> stateMachineListener() {
-			return new TestListener();
-		}
-	}
-
-	static class TestListener extends StateMachineListenerAdapter<States, Events> {
-
-		volatile CountDownLatch latch = new CountDownLatch(1);
-
-		@Override
-		public void stateChanged(State<States, Events> from, State<States, Events> to) {
-			latch.countDown();
-		}
-
-		public void reset(int count) {
-			latch = new CountDownLatch(count);
-		}
-
-	}
-
-	@SuppressWarnings("unchecked")
-	@Before
-	public void setup() {
-		context = new AnnotationConfigApplicationContext();
-		context.register(CommonConfiguration.class, Application.class, Config.class);
+	public void testApplication() {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.register(CommonConfiguration.class, Application.class);
 		context.refresh();
-		machine = context.getBean(StateMachineSystemConstants.DEFAULT_ID_STATEMACHINE, EnumStateMachine.class);
-		listener = context.getBean(TestListener.class);
+		@SuppressWarnings("unchecked")
+		StateMachine<States,Events> machine = context.getBean(StateMachineSystemConstants.DEFAULT_ID_STATEMACHINE, EnumStateMachine.class);
+
 		machine.start();
+
+		machine.sendEvent(Events.A);
+
 	}
 
 	@After
 	public void clean() {
-		machine.stop();
-		context.close();
-		context = null;
-		machine = null;
+		if (context != null) {
+			context.close();
+			context = null;
+		}
 	}
 
 }
