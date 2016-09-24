51c9e72961a53e53a803e53222aa52d83acaa519
Janne Valkealahti <janne.valkealahti@gmail.com>
2016-09-24 15:19:27 +0100

    Update docs for StateMachineComponentResolver
    - Relates to #238

--- spring-statemachine-uml/src/test/java/org/springframework/statemachine/uml/docs/DocsUmlSampleTests1.java
+++ spring-statemachine-uml/src/test/java/org/springframework/statemachine/uml/docs/DocsUmlSampleTests1.java
@@ -17,10 +17,15 @@ package org.springframework.statemachine.uml.docs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.config.EnableStateMachine;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineModelConfigurer;
+import org.springframework.statemachine.config.model.DefaultStateMachineComponentResolver;
+import org.springframework.statemachine.config.model.StateMachineComponentResolver;
 import org.springframework.statemachine.config.model.StateMachineModelFactory;
+import org.springframework.statemachine.guard.Guard;
 import org.springframework.statemachine.uml.UmlStateMachineModelFactory;
 
 public class DocsUmlSampleTests1 {
@@ -43,4 +48,53 @@ public class DocsUmlSampleTests1 {
 		}
 	}
 // end::snippetA[]
+
+// tag::snippetB[]
+	@Configuration
+	@EnableStateMachine
+	public static class Config2 extends StateMachineConfigurerAdapter<String, String> {
+
+		@Override
+		public void configure(StateMachineModelConfigurer<String, String> model) throws Exception {
+			model
+				.withModel()
+					.factory(modelFactory());
+		}
+
+		@Bean
+		public StateMachineModelFactory<String, String> modelFactory() {
+			UmlStateMachineModelFactory factory = new UmlStateMachineModelFactory(
+					"classpath:org/springframework/statemachine/uml/docs/simple-machine.uml");
+			factory.setStateMachineComponentResolver(stateMachineComponentResolver());
+			return factory;
+		}
+
+		@Bean
+		public StateMachineComponentResolver<String, String> stateMachineComponentResolver() {
+			DefaultStateMachineComponentResolver<String, String> resolver = new DefaultStateMachineComponentResolver<>();
+			resolver.registerAction("myAction", myAction());
+			resolver.registerGuard("myGuard", myGuard());
+			return resolver;
+		}
+
+		public Action<String, String> myAction() {
+			return new Action<String, String>() {
+
+				@Override
+				public void execute(StateContext<String, String> context) {
+				}
+			};
+		}
+
+		public Guard<String, String> myGuard() {
+			return new Guard<String, String>() {
+
+				@Override
+				public boolean evaluate(StateContext<String, String> context) {
+					return false;
+				}
+			};
+		}
+	}
+// end::snippetB[]
 }
