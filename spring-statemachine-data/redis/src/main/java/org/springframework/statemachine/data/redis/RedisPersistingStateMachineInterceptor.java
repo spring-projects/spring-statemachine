0a9273ef0bedfe0faed5a37cd7bcc95a544658d6
Janne Valkealahti <janne.valkealahti@gmail.com>
2017-11-29 20:29:24 +0000

    Add redis/mongo for machine persistence
    - Add similar implementations as done for JPA
      which uses redis and mongo.
    - Relates #428

---
+++ spring-statemachine-data/redis/src/main/java/org/springframework/statemachine/data/redis/RedisPersistingStateMachineInterceptor.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.statemachine.data.redis;
+
+import org.springframework.statemachine.StateMachineContext;
+import org.springframework.statemachine.data.redis.RedisRepositoryStateMachinePersist;
+import org.springframework.statemachine.data.redis.RedisStateMachineRepository;
+import org.springframework.statemachine.persist.AbstractPersistingStateMachineInterceptor;
+import org.springframework.statemachine.persist.StateMachineRuntimePersister;
+import org.springframework.statemachine.support.StateMachineInterceptor;
+import org.springframework.util.Assert;
+
+/**
+ * {@code JPA} implementation of a {@link AbstractPersistingStateMachineInterceptor}.
+ *
+ * @author Janne Valkealahti
+ *
+ * @param <S> the type of state
+ * @param <E> the type of event
+ * @param <T> the type of persister context object
+ */
+public class RedisPersistingStateMachineInterceptor<S, E, T> extends AbstractPersistingStateMachineInterceptor<S, E, T>
+		implements StateMachineRuntimePersister<S, E, T> {
+
+	private final RedisRepositoryStateMachinePersist<S, E> persist;
+
+	/**
+	 * Instantiates a new redis persisting state machine interceptor.
+	 *
+	 * @param redisStateMachineRepository the redis state machine repository
+	 */
+	public RedisPersistingStateMachineInterceptor(RedisStateMachineRepository redisStateMachineRepository) {
+		Assert.notNull(redisStateMachineRepository, "'redisStateMachineRepository' must be set");
+		this.persist = new RedisRepositoryStateMachinePersist<S, E>(redisStateMachineRepository);
+	}
+
+	/**
+	 * Instantiates a new redis persisting state machine interceptor.
+	 *
+	 * @param persist the persist
+	 */
+	public RedisPersistingStateMachineInterceptor(RedisRepositoryStateMachinePersist<S, E> persist) {
+		Assert.notNull(persist, "'persist' must be set");
+		this.persist = persist;
+	}
+
+	@Override
+	public StateMachineInterceptor<S, E> getInterceptor() {
+		return this;
+	}
+
+	@Override
+	public void write(StateMachineContext<S, E> context, T contextObj) throws Exception {
+		persist.write(context, contextObj);
+	}
+
+	@Override
+	public StateMachineContext<S, E> read(Object contextObj) throws Exception {
+		return persist.read(contextObj);
+	}
+}
---
+++ spring-statemachine-data/redis/src/main/java/org/springframework/statemachine/data/redis/RedisRepositoryStateMachinePersist.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.statemachine.data.redis;
+
+import org.springframework.statemachine.StateMachineContext;
+import org.springframework.statemachine.data.RepositoryStateMachinePersist;
+import org.springframework.statemachine.data.StateMachineRepository;
+import org.springframework.statemachine.service.StateMachineSerialisationService;
+
+/**
+ * {@code Redis} based implementation of a {@link RepositoryStateMachinePersist}.
+ *
+ * @author Janne Valkealahti
+ *
+ * @param <S> the type of state
+ * @param <E> the type of event
+ */
+public class RedisRepositoryStateMachinePersist<S, E> extends RepositoryStateMachinePersist<RedisRepositoryStateMachine, S, E> {
+
+	private final RedisStateMachineRepository redisStateMachineRepository;
+
+	/**
+	 * Instantiates a new redis repository state machine persist.
+	 *
+	 * @param redisStateMachineRepository the redis state machine repository
+	 */
+	public RedisRepositoryStateMachinePersist(RedisStateMachineRepository redisStateMachineRepository) {
+		super();
+		this.redisStateMachineRepository = redisStateMachineRepository;
+	}
+
+	/**
+	 * Instantiates a new redis repository state machine persist.
+	 *
+	 * @param redisStateMachineRepository the redis state machine repository
+	 * @param serialisationService the serialisation service
+	 */
+	public RedisRepositoryStateMachinePersist(RedisStateMachineRepository redisStateMachineRepository,
+			StateMachineSerialisationService<S, E> serialisationService) {
+		super(serialisationService);
+		this.redisStateMachineRepository = redisStateMachineRepository;
+	}
+
+	@Override
+	protected StateMachineRepository<RedisRepositoryStateMachine> getRepository() {
+		return redisStateMachineRepository;
+	}
+
+	@Override
+	protected RedisRepositoryStateMachine build(StateMachineContext<S, E> context, byte[] serialisedContext) {
+		RedisRepositoryStateMachine redisRepositoryStateMachine = new RedisRepositoryStateMachine();
+		redisRepositoryStateMachine.setMachineId(context.getId());
+		redisRepositoryStateMachine.setState(context.getState().toString());
+		redisRepositoryStateMachine.setStateMachineContext(serialisedContext);
+		return redisRepositoryStateMachine;
+	}
+}
