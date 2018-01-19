e2c2d4cbeef952f61a09f9d4e0e9692a9ba0d4aa
Janne Valkealahti <janne.valkealahti@gmail.com>
2018-01-19 08:01:48 +0000

    Fix uml spel state action
    - UmlModelParser wrongly set spel based state action as
      exit action. Change this to behave same as bean based
      action is used.
    - Fixes #474

--- spring-statemachine-uml/src/main/java/org/springframework/statemachine/uml/support/UmlModelParser.java
+++ spring-statemachine-uml/src/main/java/org/springframework/statemachine/uml/support/UmlModelParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2017 the original author or authors.
+ * Copyright 2016-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -500,7 +500,7 @@ public class UmlModelParser {
 							new SpelParserConfiguration(SpelCompilerMode.MIXED, null));
 					ArrayList<Action<String, String>> stateActions = new ArrayList<Action<String, String>>();
 					stateActions.add(new SpelExpressionAction<String, String>(parser.parseExpression(expression)));
-					stateData.setExitActions(stateActions);
+					stateData.setStateActions(stateActions);
 				}
 			}
 		}
