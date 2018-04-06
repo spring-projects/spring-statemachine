0e1c74cb85133764b7d8e91f19c33a907c6d911b
Janne Valkealahti <janne.valkealahti@gmail.com>
2018-04-06 07:24:34 +0100

    Polish

--- spring-statemachine-core/src/test/java/org/springframework/statemachine/trigger/TimerTriggerTests.java
+++ spring-statemachine-core/src/test/java/org/springframework/statemachine/trigger/TimerTriggerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2016 the original author or authors.
+ * Copyright 2015-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -57,6 +57,7 @@ public class TimerTriggerTests extends AbstractStateMachineTests {
 	public void testGetter() {
 		final long period = 1000;
 		final int count = 1;
+		@SuppressWarnings("rawtypes")
 		final TimerTrigger timerTrigger = new TimerTrigger(period, count);
 		assertEquals(period, timerTrigger.getPeriod());
 		assertEquals(count, timerTrigger.getCount());
