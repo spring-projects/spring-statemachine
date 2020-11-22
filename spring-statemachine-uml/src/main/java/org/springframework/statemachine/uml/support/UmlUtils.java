0a7067f114d639813f70414fd68bd503a336d28d
Janne Valkealahti <janne.valkealahti@gmail.com>
2020-11-22 08:51:16 +0000

    Missing spel effect with transition in uml
    - Add missing resolve/create for action if type is defined as spel.
    - Fixes #889

--- spring-statemachine-uml/src/main/java/org/springframework/statemachine/uml/support/UmlUtils.java
+++ spring-statemachine-uml/src/main/java/org/springframework/statemachine/uml/support/UmlUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2018 the original author or authors.
+ * Copyright 2016-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,9 +37,14 @@ import org.eclipse.uml2.uml.Transition;
 import org.eclipse.uml2.uml.Trigger;
 import org.eclipse.uml2.uml.UMLPackage;
 import org.eclipse.uml2.uml.resource.UMLResource;
+import org.springframework.expression.spel.SpelCompilerMode;
+import org.springframework.expression.spel.SpelParserConfiguration;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.statemachine.action.Action;
+import org.springframework.statemachine.action.SpelExpressionAction;
 import org.springframework.statemachine.config.model.StateMachineComponentResolver;
 import org.springframework.statemachine.transition.TransitionKind;
+import org.springframework.util.StringUtils;
 
 /**
  * Utilities for uml model processing.
@@ -138,9 +143,18 @@ public abstract class UmlUtils {
 		Action<String, String> action = null;
 		if (transition.getEffect() instanceof OpaqueBehavior) {
 			String beanId = UmlUtils.resolveBodyByLanguage(UmlModelParser.LANGUAGE_BEAN, (OpaqueBehavior)transition.getEffect());
-			Action<String, String> bean = resolver.resolveAction(beanId);
-			if (bean != null) {
-				action = bean;
+			if (StringUtils.hasText(beanId)) {
+				Action<String, String> bean = resolver.resolveAction(beanId);
+				if (bean != null) {
+					action = bean;
+				}
+			} else {
+				String expression = UmlUtils.resolveBodyByLanguage(UmlModelParser.LANGUAGE_SPEL, (OpaqueBehavior)transition.getEffect());
+				if (StringUtils.hasText(expression)) {
+					SpelExpressionParser parser = new SpelExpressionParser(
+							new SpelParserConfiguration(SpelCompilerMode.MIXED, null));
+					action = new SpelExpressionAction<String, String>(parser.parseExpression(expression));
+				}
 			}
 		}
 		return action;
