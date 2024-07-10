5f1a4f4eb3b6345c0034403d930514ab59df7658
Pascal Dal Farra <pascal.dalfarra@spw.wallonie.be>
2024-07-10 09:20:17 +0200

    fix: Fix TransitionComparator

--- spring-statemachine-uml/src/main/java/org/springframework/statemachine/plantuml/helper/TransitionComparator.java
+++ spring-statemachine-uml/src/main/java/org/springframework/statemachine/plantuml/helper/TransitionComparator.java
@@ -8,6 +8,12 @@ public class TransitionComparator<S, E> implements Comparator<Transition<S, E>> 
 
     @Override
     public int compare(Transition<S, E> transition1, Transition<S, E> transition2) {
-        return transition1.getSource().toString().compareTo(transition2.getTarget().toString());
+        // First compare by source
+        int compareBySource = transition1.getSource().toString().compareTo(transition2.getSource().toString());
+        if (compareBySource != 0) {
+            return compareBySource;
+        }
+        // If sources are equal, then compare by target
+        return transition1.getTarget().toString().compareTo(transition2.getTarget().toString());
     }
 }
