44ce34b85a41281969813de8a809f532a33fb72e
Mahmoud Ben Hassine <mahmoud.ben-hassine@broadcom.com>
2026-06-11 10:55:50 +0200

    Fix javadoc warning

--- spring-statemachine-core/src/main/java/org/springframework/statemachine/config/configurers/TransitionConfigurer.java
+++ spring-statemachine-core/src/main/java/org/springframework/statemachine/config/configurers/TransitionConfigurer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2021 the original author or authors.
+ * Copyright 2015-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -143,8 +143,6 @@ public interface TransitionConfigurer<T, S, E> extends
 	 *
 	 * @param name the name
 	 * @return configurer for chaining
-	 * @param name
-	 * @return
 	 */
 	T name(String name);
 }
